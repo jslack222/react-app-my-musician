8fdd4dfe22dbfece80a93df81abdac1ed000d60a
Jonathan Slack <slack6000@gmail.com>
2022-08-16 14:20:02 -0500

    20th save

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,28 +1,9 @@
-import { useEffect, useState } from 'react'
-import axios from "axios"
-import SongCard from "./SongCard"
+import React from 'react'
 
 const Music = () => {
-    const [songRender, setSongRender] = useState()
-
-    useEffect(() => {
-      axios.get("/getsongs").then((res) => {
-        setSongRender(res.data);
-      });
-    }, []);
-    console.log(songRender);
-
-    const songDisplay = (songs) => {
-      return (
-        <SongCard
-          songs={songs.tracks}
-        />
-      );
-    };
     return (
         <div className="musicContainer">
             <h1>This is the Music Page</h1>
-            {songDisplay}
         </div>
     )
 }
